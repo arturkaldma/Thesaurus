39432a8f84f5cfd89fb0350c102cef6a177cb4f3
arturkaldma <arturkaldma@gmail.com>
2021-03-18 15:53:13 +0200

    Global update for user-friendly output.

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import json
+from difflib import get_close_matches
+data = json.load(open("data.json"))
 def get_answer(key):
-    data = json.load(open("data.json"))
-    return data[key]
+    if key in data:
+        return ' '.join([str(el) for el in data[key]])
+    elif key.title() in data:
+        return ' '.join([str(el) for el in data[key.title()]])
+    elif key.upper() in data:
+        return ' '.join([str(el) for el in data[key.upper()]])
+    elif len(get_close_matches(key, data.keys())) > 0:
+        w = get_close_matches(key, data.keys())[0]
+        print("Did you mean %s instead? If yes type Y, if not type N" % w)
+        a = input("Y/N: ")
+        if a == "Y":
+            return ' '.join([str(el) for el in data[w]])
+        elif a == "N":
+            return "Something is wrong! Try write the word correctly!"
+        else:
+            return "Wrong entry"
+    else:
+        return "Something is wrong! Try write the word correctly!"
 
-word = input("The word I'm looking for: ")
-l = get_answer(word)
-answer = ' '.join([str(el) for el in l])
-print(answer)
+word = input("The word I'm looking for: ").lower()
+print(get_answer(word))

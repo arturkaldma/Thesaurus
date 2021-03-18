2c16d400b916804ee42f6782f6b8b4b3f66477de
arturkaldma <arturkaldma@gmail.com>
2021-03-18 14:11:40 +0200

    Improved output text from list to string.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
-
 def get_answer(key):
     data = json.load(open("data.json"))
     return data[key]
 
-print(get_answer("rain"))
+word = input("The word I'm looking for: ")
+l = get_answer(word)
+answer = ' '.join([str(el) for el in l])
+print(answer)
